81e200d21ac2cd06383f3bad986f3c9b7c740bf7
Junio Eduvirgem <junio.eduvirgem@ebserh.gov.br>
2022-09-25 09:34:51 -0400

    Mudou a comparação

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,7 +7,8 @@ numero_secreto = 63
 chute = input ("Digite o seu numero: ")
 
 print ("Você digitou", chute)
-comparacao = numero_secreto == int (chute)
+chute_convertido = int (chute)
+comparacao = numero_secreto == int (chute_convertido)
 print ("o valor da comparação é:", comparacao)
 if (comparacao) : 
     print ("você acertou!")
