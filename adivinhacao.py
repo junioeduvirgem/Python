271b90d0988ad95adb324b565cc8cd76e773edfe
Junio Eduvirgem <junio.eduvirgem@ebserh.gov.br>
2022-09-25 09:37:03 -0400

    Revert "Mudou a comparação"
    This reverts commit 81e200d21ac2cd06383f3bad986f3c9b7c740bf7.

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,8 +7,7 @@ numero_secreto = 63
 chute = input ("Digite o seu numero: ")
 
 print ("Você digitou", chute)
-chute_convertido = int (chute)
-comparacao = numero_secreto == int (chute_convertido)
+comparacao = numero_secreto == int (chute)
 print ("o valor da comparação é:", comparacao)
 if (comparacao) : 
     print ("você acertou!")
